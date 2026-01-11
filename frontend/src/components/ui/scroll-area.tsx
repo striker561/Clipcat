98f7969805b7df096cfdeaae363fa695089a75d4
d3uceY <onyekwelujesse1234@gmail.com>
2026-01-11 11:09:30 +0100

    feat: Add cursor-move class to ScrollAreaThumb for better UX

--- frontend/src/components/ui/scroll-area.tsx
+++ frontend/src/components/ui/scroll-area.tsx
@@ -47,7 +47,7 @@ function ScrollBar({
     >
       <ScrollAreaPrimitive.ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="bg-black blur-[2px] relative flex-1 rounded-full"
+        className="bg-black blur-[2px] relative flex-1 rounded-full cursor-move"
       />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   )
