5811a6949dff4364ca88b7072c0c03dd4920b7b1
d3uceY <onyekwelujesse1234@gmail.com>
2026-01-28 11:16:41 +0100

    feat: add sound effect for delete button hover action

--- frontend/src/components/delete-button.tsx
+++ frontend/src/components/delete-button.tsx
@@ -1,12 +1,14 @@
 import { gsap } from "gsap";
 import { useRef } from "react";
+import { playSound } from "@/helpers/playSound";
+import { useClips } from "@/context/ClipContext";
 
 export default function DeleteButton({ onClick }: { onClick: () => void }) {
-
+    const { soundOn } = useClips();
     const tl = gsap.timeline();
-     const deleteBtnRef = useRef<HTMLButtonElement>(null);
-     const deleteImgRef = useRef<HTMLImageElement>(null);
-     const deleteImgRedRef = useRef<HTMLImageElement>(null);
+    const deleteBtnRef = useRef<HTMLButtonElement>(null);
+    const deleteImgRef = useRef<HTMLImageElement>(null);
+    const deleteImgRedRef = useRef<HTMLImageElement>(null);
 
     // Animation tween for hover effect 
     const tween = () =>
@@ -21,7 +23,10 @@ export default function DeleteButton({ onClick }: { onClick: () => void }) {
 
 
     return (
-        <button onClick={onClick} onMouseEnter={() => tween().play()} onMouseLeave={() => tween().reverse(0.1)} className="py-3  block w-full" ref={deleteBtnRef}>
+        <button onClick={onClick} onMouseEnter={() => {
+            playSound('/sounds/trash.mp3', soundOn, 1);
+            tween().play();
+        }} onMouseLeave={() => tween().reverse(0.1)} className="py-3  block w-full" ref={deleteBtnRef}>
             <img src="/delete-base.png" alt="" className="h-8 block mx-auto delete-btn" ref={deleteImgRef} />
             <img src="/delete-red.png" alt="" className="h-8 mx-auto delete-btn-red hidden" ref={deleteImgRedRef} />
         </button>
