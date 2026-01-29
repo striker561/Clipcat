dd71747fdb16ec4c8469240db5075676d9ce048a
d3uceY <onyekwelujesse1234@gmail.com>
2026-01-29 10:14:33 +0100

    chore: update version to 0.5.2 in README, wails_tools, main.go, and wails.json

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-const AppVersion = "v0.5.1"
+const AppVersion = "v0.5.2"
 
 func main() {
 	// Create an instance of the app structure
