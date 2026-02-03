cf3c78d8ce1e9958cc8eb01f32594c44f81955da
d3uceY <onyekwelujesse1234@gmail.com>
2026-02-03 23:05:55 +0100

    chore: update version to 0.7.0 in README, wails_tools, main.go, and wails.json

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-const AppVersion = "v0.6.1"
+const AppVersion = "v0.7.0"
 
 func main() {
 	// Create an instance of the app structure
