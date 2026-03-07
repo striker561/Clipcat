87dbb24b575ae98975af2a922a21f3c18649e98a
d3uceY <onyekwelujesse1234@gmail.com>
2026-03-07 12:51:42 +0100

    chore: update version to 0.8.1 in README.md, wails_tools.nsh, main.go, and wails.json

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-const AppVersion = "v0.8.0"
+const AppVersion = "v0.8.1"
 
 func main() {
 	// Create an instance of the app structure
